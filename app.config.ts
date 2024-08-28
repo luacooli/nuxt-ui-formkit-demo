11cf1eb42a8f2a5abef16eba5e480fe9ed9e492e
Luana Correia <luacooli@gmail.com>
2024-08-28 17:20:48 -0300

    feat: fields with tailwind

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+	ui: {
+		primary: 'lime',
+		gray: 'neutral',
+		notifications: {
+			position: 'bottom-0 right-0',
+		},
+	},
+});
---
+++ components/new-patient/select-data.ts
@@ -0,0 +1,29 @@
+export const document_types = [
+	{ label: 'RG', value: 'RG' },
+	{ label: 'CPF', value: 'CPF' },
+	{ label: 'CNH', value: 'CNH' },
+];
+
+export const marital_status = [
+	{ label: 'Solteiro(a)', value: 'Solteiro' },
+	{ label: 'Casado(a)', value: 'Casado' },
+];
+
+export const skin_color = [
+	{ label: 'Preto', value: 'Preto' },
+	{ label: 'Pardo', value: 'Pardo' },
+	{ label: 'Branco', value: 'Branco' },
+	{ label: 'Indígena', value: 'Indígena' },
+];
+
+export const health_plan = [
+	{ label: 'Unimed', value: 'Unimed' },
+	{ label: 'Samp', value: 'Samp' },
+	{ label: 'Bradesco', value: 'Bradesco' },
+	{ label: 'Amil', value: 'Amil' },
+];
+
+export const sex = [
+	{ label: 'Feminino', value: 'Feminino' },
+	{ label: 'Masculino', value: 'Masculino' },
+];
